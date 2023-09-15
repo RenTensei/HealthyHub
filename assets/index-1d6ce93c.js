387c2ebdbd2c72d58403f2b0682354efcf7283ba
RenTensei <scs.bon7@gmail.com>
2023-09-15 19:35:14 +0000

    Deploying to gh-pages from @ RenTensei/HealthyHub@866eba1c0e83b7c4b1988c6c0a24720eaa0d947a 🚀

---
+++ assets/index-1d6ce93c.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-df824d96.js";const r=()=>a.jsx(a.Fragment,{children:"dashboard page"}),e=r;export{e as default};
---
+++ assets/index-e4a55c34.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-df824d96.js";const t=()=>e.jsx(e.Fragment,{children:"profile settings page"}),s=t;export{s as default};

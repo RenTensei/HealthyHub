7dd80e3cbca9a91320f039d7b17b7e8ab8be4c57
VernihoraIvan <vernihorai@gmail.com>
2023-09-16 19:29:17 +0200

    update

--- src/pages/MainPage/DailyGoal/DailyGoal.jsx
+++ src/pages/MainPage/DailyGoal/DailyGoal.jsx
@@ -1,10 +1,31 @@
-// import styles from './DailyGoal.module.scss';
+import styles from './DailyGoal.module.scss';
+
+import CaloriesSVG from './svgImages/calories';
+import WaterSVG from './svgImages/water';
 
 const DailyGoal = () => {
   return (
-    <>
-      <div>Set goals and achieve them</div>
-    </>
+    <div>
+      <h2>Daily goal</h2>
+      <div className={styles.goal_container}>
+        <div className={styles.goal_wrapper}>
+          <div className={styles.daily_goal_container}>
+            <CaloriesSVG />
+            <div>
+              <h3>Calories</h3>
+              <p>1100</p>
+            </div>
+          </div>
+          <div className={styles.daily_goal_container}>
+            <WaterSVG />
+            <div>
+              <h3>Water</h3>
+              <p>500</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 

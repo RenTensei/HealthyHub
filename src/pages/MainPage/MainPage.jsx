75f2217d14f2d7f5f4ff0a3d6ac272f3eeb6396b
VernihoraIvan <vernihorai@gmail.com>
2023-09-17 13:49:22 +0200

    RecordMeal and WaterIntake modal windows markup added

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,10 +2,19 @@ import Diary from './components/Diary';
 import Water from './components/Water';
 import Food from './components/Food';
 import DailyGoal from './DailyGoal';
+import ModalWaterIntake from './Modals/ModalWaterIntake/ModalWaterIntake';
+import ModalRecordMeal from './Modals/ModalRecordMeal/ModalRecordMeal';
 
 const MainPage = () => {
   return (
     <>
+      <ModalRecordMeal
+        meal="Breakfast"
+        srcImg="/Breakfast.png"
+        srcSet="/Breakfast.png, /Breakfast@2x.png"
+        alt="Breakfast"
+      />
+      <ModalWaterIntake />
       <DailyGoal />
       <Water />
       <Food />

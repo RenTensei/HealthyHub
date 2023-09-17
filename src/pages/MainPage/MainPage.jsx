ef70d0348ea0f660560766de0dfea994c564c54c
VernihoraIvan <vernihorai@gmail.com>
2023-09-17 18:44:27 +0200

    added modals for mainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,24 +2,27 @@ import Diary from './components/Diary';
 import Water from './components/Water';
 import Food from './components/Food';
 import DailyGoal from './DailyGoal';
-import ModalWaterIntake from './Modals/ModalWaterIntake/ModalWaterIntake';
-import ModalRecordMeal from './Modals/ModalRecordMeal/ModalRecordMeal';
+// import ModalWaterIntake from './Modals/ModalWaterIntake/ModalWaterIntake';
+// import ModalRecordMeal from './Modals/ModalRecordMeal/ModalRecordMeal';
+import styles from './MainPAge.module.scss';
 
 const MainPage = () => {
   return (
-    <>
-      <ModalRecordMeal
+    <div className={styles.container}>
+      {/* <ModalRecordMeal
         meal="Breakfast"
         srcImg="/Breakfast.png"
         srcSet="/Breakfast.png, /Breakfast@2x.png"
         alt="Breakfast"
       />
-      <ModalWaterIntake />
-      <DailyGoal />
-      <Water />
-      <Food />
+      <ModalWaterIntake /> */}
+      <div className={styles.components_container}>
+        <DailyGoal />
+        <Water />
+        <Food />
+      </div>
       <Diary />
-    </>
+    </div>
   );
 };
 
--- src/pages/MainPage/components/Water/Water.jsx
+++ src/pages/MainPage/components/Water/Water.jsx
@@ -4,10 +4,10 @@ import Bar from '../Bar';
 
 const Water = () => {
   return (
-    <>
+    <div className={styles.container}>
       <h2 className={styles.title}>Water</h2>
       <Bar waterConsumption={11110} />
-    </>
+    </div>
   );
 };
 

e3389c88d800ce5a62843c9b62d66b7b8c4993fa
Voloshun555 <94966717+Voloshun555@users.noreply.github.com>
2023-09-18 22:18:18 +0300

    555

--- src/pages/DiaryPage/ components/DiaryBlock/DiaryBlock.jsx
+++ src/pages/DiaryPage/ components/DiaryBlock/DiaryBlock.jsx
@@ -1,52 +1,112 @@
 import styles from './DiaryBlock.module.scss';
+import { nanoid } from 'nanoid';
 
 const DiaryBlock = ({
-  img,
   alt,
   title,
   Carbonohidrates = '0',
   Protein = '0',
   fat = '0',
   product,
+  srcSet,
 }) => {
+
+  const imgSrc = srcSet.split(' ')[0];
+  // const breakfast = [{
+  //   mealName: 'spaghetti',
+  //   carbonohidrates: 54,
+  //   protein: 30,
+  //   fat: 14
+  // }]
+  
+  // const dinner = [
+  //   {
+  //     mealName: 'spaghetti',
+  //     carbonohidrates: 54,
+  //     protein: 30,
+  //     fat: 14,
+  //   },
+  // ];
+  // const lunch = [
+  //   {
+  //     mealName: 'spaghetti',
+  //     carbonohidrates: 54,
+  //     protein: 30,
+  //     fat: 14,
+  //   },
+  // ];
+  // const snack = [
+  //   {
+  //     mealName: 'spaghetti',
+  //     carbonohidrates: 54,
+  //     protein: 30,
+  //     fat: 14,
+  //   },
+  // ];
+
+  // const mealTypes = [
+  //   { type: 'Breakfast', data: breakfast },
+  //   { type: 'Dinner', data: dinner },
+  //   { type: 'Lunch', data: lunch },
+  //   { type: 'Snack', data: snack },
+  // ];
+
+
+
+
+
+
+
+
+
   return (
     <div>
       <div className={styles.containerBlockHeader}>
-      <div className={styles.blockHeader}>
-        <img src={img} height={32} width={32} alt={alt} />
-        <h2 className={styles.titleBlockHeader}>{title}</h2>
-      </div>
-      <div className={styles.itemCalories}>
-        <p className={styles.item_1}>
-          Carbonohidrates: <span className={styles.caloriesSum}>{Carbonohidrates}</span>
-        </p>
-        <p className={styles.item_2}>
-          Protein: <span className={styles.caloriesSum}>{Protein}</span>
-        </p>
-        <p className={styles.item_3}>
-          Fat: <span className={styles.caloriesSum}>{fat}</span>
-        </p>
-      </div>
+        <div className={styles.blockHeader}>
+          <img src={imgSrc} srcSet={srcSet} height={32} width={32} alt={alt} />
+          <h2 className={styles.titleBlockHeader}>{title}</h2>
+        </div>
+        <div className={styles.itemCalories}>
+          <p className={styles.item_1}>
+            Carbonohidrates:
+            <span className={styles.caloriesSum}>{Carbonohidrates}</span>
+          </p>
+          <p className={styles.item_2}>
+            Protein: <span className={styles.caloriesSum}>{Protein}</span>
+          </p>
+          <p className={styles.item_3}>
+            Fat: <span className={styles.caloriesSum}>{fat}</span>
+          </p>
+        </div>
       </div>
       <div className={styles.blocks}>
-        <div className={styles.blockItems}>
-          <ul className={styles.blockItemsGrup}>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-          </ul>
-        </div>
-        <ul className={styles.listItems}>
-          <li className={styles.listProduct}>
-            {!product && (
-              <button className={styles.button}>+ Record your meal</button>
-            )}
-          </li>
-          <li className={styles.listProduct}>{product}</li>
-        </ul>
+       <div className={styles.blockItems}>
+<ul className={styles.blockItemsGrup}>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+  <li>4</li>
+</ul>
+</div>
+<div className={styles.listItems}>
+<div className={styles.listProduct}>
+  {!product ?  (
+    <button className={styles.button}>+ Record your meal</button>
+  ) : <div ><div className={styles.containerFoodName}> <h2 className={styles.foodName}>{product}</h2> <button className={styles.bettonEdit}>Edit</button></div> 
+   <div className={styles.caloriesProduct}>
+  <p>Carb.</p>
+  <p>Prot.</p>
+  <p>Fat.</p>
+  </div>
+  </div> }
+</div>
+</div>
+
       </div>
     </div>
   );
 };
 export default DiaryBlock;
+
+
+ 
--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,8 +1,19 @@
 import { Link } from 'react-router-dom';
 import DiaryBlock from './ components/DiaryBlock';
 import styles from './DiaryPage.module.scss';
+import { useSelector } from 'react-redux';
+import { selectNutrientSums } from '@/store/features/foodIntake/selectors';
 
 const DiaryPage = () => {
+  const FoodIntakeNutrientsTotal = useSelector(selectNutrientSums);
+ 
+  const mealTypeSrcSets = {
+    Breakfast: 'public/Breakfast.png 1x, public/Breakfast@2x.png 2x',
+    Lunch: 'public/Lunch.png 1x, public/Lunch@2x.png 2x',
+    Dinner: 'public/Dinner.png 1x, public/Dinner@2x.png 2x ',
+    Snack: 'public/Snack.png 1x, public/Snack@2x.png 2x',
+  };
+
   return (
     <section className={styles.sectionDiary}>
       <div className={styles.containerDiary}>
@@ -15,19 +26,31 @@ const DiaryPage = () => {
           Diary
         </Link>
         <div className={styles.containerBloks}>
-        <DiaryBlock
-        className={styles.containerBloks_1}
-          img="/Breakfast.png"
-          srcSet="/Breakfast.png, /Breakfast@2x.png"
-          title={'Breakfast'}
-          product={'kklklk'}
-        />
-        <DiaryBlock className={styles.containerBloks_2}  img="/Lunch.png" srcSet="/Lunch.png, /Lunch@2x.png" />
-        <DiaryBlock className={styles.containerBloks_3} img="/Dinner.png" srcSet="/Dinner.png, /Dinner@2x.png" />
-        <DiaryBlock className={styles.containerBloks_4} img="/Snack.png" srcSet="/Snack.png, /Snack@2x.png" />
+          {FoodIntakeNutrientsTotal.map(item => (
+            <DiaryBlock
+            product={"llll"}
+              key={item.mealType}
+              data={FoodIntakeNutrientsTotal}
+              title={item.mealType}
+              srcSet={mealTypeSrcSets[item.mealType]}
+            />
+          ))}
         </div>
       </div>
     </section>
   );
 };
 export default DiaryPage;
+
+{
+  /* <DiaryBlock
+        className={styles.containerBloks_1}
+          img="/Breakfast.png"
+          srcSet="/Breakfast.png, /Breakfast@2x.png"
+          title={'Breakfast'}
+          product={'Pasta'}
+        />
+        <DiaryBlock className={styles.containerBloks_2}  img="/Lunch.png" srcSet="/Lunch.png, /Lunch@2x.png" title={'Lunch'}/>
+        <DiaryBlock className={styles.containerBloks_3} img="/Dinner.png" srcSet="/Dinner.png, /Dinner@2x.png" title={'Dinner'}/>
+        <DiaryBlock className={styles.containerBloks_4} img="/Snack.png" srcSet="/Snack.png, /Snack@2x.png" title={'Snack'}/> */
+}

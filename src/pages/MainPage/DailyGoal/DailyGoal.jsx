9d55e526b25cd081648861f2409a6d28d4f85197
RenTensei <scs.bon7@gmail.com>
2023-09-19 13:34:50 +0300

    + responsive main

--- src/pages/MainPage/DailyGoal/DailyGoal.jsx
+++ src/pages/MainPage/DailyGoal/DailyGoal.jsx
@@ -5,24 +5,24 @@ import WaterSVG from './svgImages/waterSVG';
 
 const DailyGoal = () => {
   return (
-    <div>
-      <h2 className={styles.daily_title}>Daily goal</h2>
+    <div className={styles.daily_goal_block}>
+      <h3 className={styles.daily_goal_title}>Daily goal</h3>
+
       <div className={styles.goal_container}>
-        <div className={styles.goal_wrapper}>
-          <div className={styles.daily_goal_container}>
-            <CaloriesSVG />
-            <div>
-              <h3>Calories</h3>
-              <p>1700</p>
-            </div>
+        <div className={styles.daily_goal_container}>
+          <CaloriesSVG />
+          <div>
+            <h4>Calories</h4>
+            <p>1700</p>
           </div>
-          <div className={styles.daily_goal_container}>
-            <WaterSVG />
-            <div>
-              <h3>Water</h3>
-              <div className={styles.water_container}>
-                1500 <span className={styles.ml}> ml</span>
-              </div>
+        </div>
+
+        <div className={styles.daily_goal_container}>
+          <WaterSVG />
+          <div>
+            <h4>Water</h4>
+            <div className={styles.water_container}>
+              1500 <span className={styles.ml}> ml</span>
             </div>
           </div>
         </div>

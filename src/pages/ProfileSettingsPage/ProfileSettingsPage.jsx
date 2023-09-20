2ed7bb98f34579f15ad011dc452ca1735e6a3556
Promart1 <marta.batih@gmail.com>
2023-09-20 17:04:09 +0300

    fix bug

--- src/pages/ProfileSettingsPage/ProfileSettingsPage.jsx
+++ src/pages/ProfileSettingsPage/ProfileSettingsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import { activityLevels } from './activityLevel';
+import { Formik, Field, Form, ErrorMessage, useFormik } from 'formik';
+import { physicalActivityRatio } from './activityLevel';
 import ProfileSettingsSchema from './ProfileSettingsSchema';
 import picture from './image/Setting.png';
 
@@ -26,8 +26,8 @@ function ProfileSettingsPage() {
   //   height: '',
   //   gender: '',
   //   weight: '',
-  //   activityLevel: '',
-  //   photo: null,
+  //   physicalActivityRatio: '',
+  //
   // });
 
   // const [values, setValues] = useState({ ...initialValues });
@@ -46,8 +46,11 @@ function ProfileSettingsPage() {
   };
 
   const handlePhotoChange = (e, setFieldValue) => {
-    const photo = e.target.files[0];
-    setFieldValue('photo', photo ? URL.createObjectURL(photo) : defaultPhoto);
+    const avatarURL = e.target.files[0];
+    setFieldValue(
+      ' avatarURL',
+      avatarURL ? URL.createObjectURL(avatarURL) : defaultPhoto
+    );
   };
 
   return (
@@ -55,131 +58,145 @@ function ProfileSettingsPage() {
       <div className={style.containerTitle}>
         <h2>Profile setting</h2>
         <div>
+          <img src={picture} alt="Picture" className={style.mainImg}></img>
+        </div>
+        {/* <div>
           <button className={style.cancelBtn} onClick={handleCancel}>
             Cancel
           </button>
           <button type="submit" className={style.saveBtn} onClick={handleSave}>
             Save
           </button>
-        </div>
+        </div> */}
       </div>
-      <div className={style.containerContent}>
-        <div>
+      {/* <div className={style.containerContent}> */}
+      {/* <div>
           <img src={picture} alt="Picture" className={style.mainImg}></img>
-        </div>
-        <div className={style.containerSettings}>
-          <Formik
-            initialValues={userInfo}
-            validationSchema={ProfileSettingsSchema}
-            onSubmit={handleSave}
-          >
-            {({ values, setFieldValue }) => (
-              <Form>
-                <div className={style.containerSetting}>
-                  <div className={style.containerSettingFirst}>
-                    <label>
-                      Your name
-                      <Field type="text" name="name" />
-                      <ErrorMessage name="name" component="div" />
-                    </label>
+        </div> */}
+      <div className={style.containerSettings}>
+        <Formik
+          initialValues={userInfo}
+          validationSchema={ProfileSettingsSchema}
+          onSubmit={handleSave}
+        >
+          {({ values, setFieldValue }) => (
+            <Form>
+              <div className={style.button}>
+                <button className={style.cancelBtn} onClick={handleCancel}>
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className={style.saveBtn}
+                  onClick={handleSave}
+                >
+                  Save
+                </button>
+              </div>
+              <div className={style.containerSetting}>
+                <div className={style.containerSettingFirst}>
+                  <label>
+                    Your name
+                    <Field type="text" name="name" />
+                    <ErrorMessage name="name" component="div" />
+                  </label>
 
-                    <label>
-                      Your age
-                      <Field type="number" name="age" min="0" />
-                      <ErrorMessage name="age" component="div" />
-                    </label>
+                  <label>
+                    Your age
+                    <Field type="number" name="age" min="0" />
+                    <ErrorMessage name="age" component="div" />
+                  </label>
 
-                    <label>
-                      Height
-                      <Field type="number" name="height" min="0" />
-                      <ErrorMessage name="height" component="div" />
-                    </label>
-                  </div>
-                  <div>
-                    <label className={style.text}>Your photo</label>
-                    <div className={style.containerPhoto}>
-                      <div>
-                        <img
-                          src={values.photo || defaultPhoto}
-                          // src={avatar}
-                          alt="Profile"
-                          style={{ width: '36px', height: '36px' }}
-                          className={style.photo}
-                        />
-                      </div>
-
-                      <AddPhotoSvg
-                        className={style.svg}
-                        width={16}
-                        height={16}
-                      />
-                      <label htmlFor="photo">Download new photo</label>
-                      <input
-                        className={style.inputPhoto}
-                        type="file"
-                        id="photo"
-                        accept="image/*"
-                        onChange={e => handlePhotoChange(e, setFieldValue)}
-                      />
-                    </div>
-
-                    <label className={style.text}>Gender</label>
-                    <div className={style.contairGender}>
-                      <label>
-                        <Field type="radio" name="gender" value="male" />
-                        Male
-                      </label>
-
-                      <label>
-                        <Field type="radio" name="gender" value="female" />
-                        Female
-                      </label>
-                    </div>
-                    <label className={style.text}>Weight</label>
-                    <div className={style.containerWeight}>
-                      <label>
-                        <Field type="number" name="weight" min="0" />
-                        <ErrorMessage name="weight" component="div" />
-                      </label>
-                    </div>
-                  </div>
+                  <label>
+                    Height
+                    <Field type="number" name="height" min="0" />
+                    <ErrorMessage name="height" component="div" />
+                  </label>
                 </div>
                 <div>
-                  <div className={style.containerActivity}>
-                    <label htmlFor="activityLevel" className={style.text}>
-                      Your activity
+                  <label className={style.text}>Your photo</label>
+                  <div className={style.containerPhoto}>
+                    <div>
+                      <img
+                        src={values.avatarURL || defaultPhoto}
+                        // src={avatar}
+                        alt="Profile"
+                        style={{ width: '36px', height: '36px' }}
+                        className={style.photo}
+                      />
+                    </div>
+
+                    <AddPhotoSvg className={style.svg} width={16} height={16} />
+                    <label htmlFor="avatarURL">Download new photo</label>
+                    <input
+                      className={style.inputPhoto}
+                      type="file"
+                      id="avatarURL"
+                      accept="image/*"
+                      onChange={e => handlePhotoChange(e, setFieldValue)}
+                    />
+                  </div>
+
+                  <label className={style.text}>Gender</label>
+                  <div className={style.contairGender}>
+                    <label>
+                      <Field
+                        type="radio"
+                        name="gender"
+                        value="Female"
+                        checked={values.gender === 'Male'}
+                      />
+                      Male
                     </label>
-                    {activityLevels.map(level => (
-                      <div key={level.value}>
-                        <Field
-                          type="radio"
-                          id={`activityLevel-${level.value}`}
-                          name="activityLevel"
-                          value={level.value}
-                          className={style.boxActivity}
-                        />
-                        <label
-                          className={style.text}
-                          htmlFor={`activityLevel-${level.value}`}
-                        >
-                          {level.value} - {level.label}
-                        </label>
-                      </div>
-                    ))}
+
+                    <label>
+                      <Field
+                        type="radio"
+                        name="gender"
+                        value="Female"
+                        checked={values.gender === 'Female'}
+                      />
+                      Female
+                    </label>
+                  </div>
+                  <label className={style.text}>Weight</label>
+                  <div className={style.containerWeight}>
+                    <label>
+                      <Field type="number" name="weight" min="0" />
+                      <ErrorMessage name="weight" component="div" />
+                    </label>
                   </div>
                 </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
-      </div>
-      <div className={style.mobileBtn}>
-        <button type="submit" className={style.saveBtnM} onClick={handleSave}>
-          Save
-        </button>
-        <button className={style.cancelBtnM} onClick={handleCancel}>
-          Cancel
-        </button>
+              </div>
+              <div>
+                <div className={style.containerActivity}>
+                  <label htmlFor="physicalActivityRatio" className={style.text}>
+                    Your activity
+                  </label>
+                  {physicalActivityRatio.map(level => (
+                    <div key={level.value}>
+                      <Field
+                        type="radio"
+                        id={`physicalActivityRatio-${level.value}`}
+                        name="physicalActivityRatio"
+                        value={level.value}
+                        className={style.boxActivity}
+                        checked={values.physicalActivityRatio === level.value}
+                      />
+                      <label
+                        className={style.text}
+                        htmlFor={`physicalActivityRatio-${level.value}`}
+                      >
+                        {level.value} - {level.label}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
+        {/* </div> */}
       </div>
     </div>
   );

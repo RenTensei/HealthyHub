0876de9db9057279a02a02cd58d24fb0b612bf17
Bondarev Andrii <scs.bon7@gmail.com>
2023-09-20 19:32:10 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/HealthyHub/">
+      <BrowserRouter basename="/HealthyHub">
         <ModalProvider>
           <ModalManager />
           <App />

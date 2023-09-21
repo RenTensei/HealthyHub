cfcc342beb2fb4641480c7bdddffbc0adfd2def5
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-09-21 10:54:46 +0300

    add important

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -8,82 +8,28 @@ const WelcomePage = lazy(() => import('@/pages/WelcomePage'));
 const MainPage = lazy(() => import('@/pages/MainPage'));
 const SignUpPage = lazy(() => import('@/pages/SignUpPage'));
 const SignInPage = lazy(() => import('@/pages/SignInPage'));
-const ForgotYourPasswordPage = lazy(() => import('@/pages/ForgotPasswordPage'));
 const DiaryPage = lazy(() => import('@/pages/DiaryPage'));
 const RecommendedFoodPage = lazy(() => import('@/pages/RecommendedFoodPage'));
 const DashboardPage = lazy(() => import('@/pages/DashboardPage'));
 const ProfileSettingsPage = lazy(() => import('@/pages/ProfileSettingsPage'));
 
-import { PublicRoute } from './PublicRoute';
-import { PrivateRoute } from './PrivateRoute';
-
 const Router = () => {
   return (
     <Routes>
       <Route path={ROUTES.HomePage} element={<SharedLayout />}>
-        {/* public routes */}
+        <Route index element={<WelcomePage />} />
+        <Route path={ROUTES.MainPage} element={<MainPage />} />
+        <Route path={ROUTES.SignUpPage} element={<SignUpPage />} />
+        <Route path={ROUTES.SignInPage} element={<SignInPage />} />
+        <Route path={ROUTES.DiaryPage} element={<DiaryPage />} />
         <Route
-          index
-          element={
-            <PublicRoute component={WelcomePage} redirect={ROUTES.MainPage} />
-          }
-        />
-        <Route
-          path={ROUTES.SignUpPage}
-          element={
-            <PublicRoute component={SignUpPage} redirect={ROUTES.MainPage} />
-          }
-        />
-        <Route
-          path={ROUTES.SignInPage}
-          element={
-            <PublicRoute component={SignInPage} redirect={ROUTES.MainPage} />
-          }
-        />
-
-        {/* private routes */}
-        <Route
-          path={ROUTES.MainPage}
-          element={
-            <PrivateRoute component={MainPage} redirect={ROUTES.SignInPage} />
-          }
-        />
-        <Route
-          path={ROUTES.ForgotYourPasswordPage}
-          element={
-            <PrivateRoute
-              component={ForgotYourPasswordPage}
-              redirect={ROUTES.SignInPage}
-            />
-          }
-        />
-        <Route
-          path={ROUTES.DiaryPage}
-          element={
-            <PrivateRoute component={DiaryPage} redirect={ROUTES.SignInPage} />
-          }
-        />
-         <Route
           path={ROUTES.RecommendedFoodPage}
-          element={<PrivateRoute component={RecommendedFoodPage} redirect={ROUTES.SignInPage} />}
+          element={<RecommendedFoodPage />}
         />
-        <Route
-          path={ROUTES.DashboardPage}
-          element={
-            <PrivateRoute
-              component={DashboardPage}
-              redirect={ROUTES.SignInPage}
-            />
-          }
-        />
+        <Route path={ROUTES.DashboardPage} element={<DashboardPage />} />
         <Route
           path={ROUTES.ProfileSettingsPage}
-          element={
-            <PrivateRoute
-              component={ProfileSettingsPage}
-              redirect={ROUTES.SignInPage}
-            />
-          }
+          element={<ProfileSettingsPage />}
         />
       </Route>
     </Routes>

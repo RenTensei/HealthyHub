368af63786d8db6374c62cf193c5d0646729adfa
TatyanaVoloshko <106927709+TatyanaVoloshko@users.noreply.github.com>
2023-09-25 17:45:14 +0200

    added success state for signUpForm

--- src/pages/SignUpPage/components/SignUpFormPage/components/SignUpForm/SignUpForm.jsx
+++ src/pages/SignUpPage/components/SignUpFormPage/components/SignUpForm/SignUpForm.jsx
@@ -16,23 +16,17 @@ const initialValues = {
 
 const SignUpForm = () => {
   const [visible, setVisible] = useState(true);
-  const [success, setSuccess] = useState(false);
+  const [inputState, setInputState] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
 
   const { nextStage, addSignUpData } = useSignUpContext();
 
   const handleSubmit = (values, { resetForm }) => {
-    // console.log(values);
-
     addSignUpData(values);
     nextStage();
-
-    // setTimeout(() => {
-    //   setSuccess(true);
-    //   resetForm();
-    //   setTimeout(() => {
-    //     setSuccess(false);
-    //   }, 3000);
-    // }, 1000);
   };
 
   return (
@@ -41,7 +35,7 @@ const SignUpForm = () => {
       validationSchema={validationSchemaSignUp}
       onSubmit={handleSubmit}
     >
-      {({ errors, touched }) => (
+      {({ errors, touched, setFieldTouched }) => (
         <Form autoComplete="off" className={styles.Form}>
           <label htmlFor="name" className={styles.Label}></label>
           <div className={styles.Input_wrapper}>
@@ -50,14 +44,42 @@ const SignUpForm = () => {
               name="name"
               placeholder="Name"
               className={`${styles.Input} ${
-                errors.name && touched.name ? styles.Input__error : null
+                errors.name && touched.name
+                  ? styles.Input__error
+                  : inputState.name && !errors.name
+                  ? styles.Input__success
+                  : null
               }`}
+              onInput={e => {
+                const value = e.target.value;
+                setInputState(prevState => ({
+                  ...prevState,
+                  name: value,
+                }));
+                setFieldTouched('name', true); // Устанавливаем touched в true
+              }}
             />
             <ErrorMessage
               className={`${styles.Message} ${styles.Message__error}`}
               name="name"
               component="div"
             />
+            {errors.name && touched.name ? (
+              <div
+                className={`${styles.Icon} 
+                  ${styles.Icon__right} 
+                  ${styles.Icon__error} 
+                  ${styles.Icon__right_secondary}`}
+              >
+                <ErrorLogoSvg width={24} height={24} />
+              </div>
+            ) : inputState.name && !errors.name ? (
+              <div
+                className={`${styles.Icon} ${styles.Icon__right} ${styles.Icon__success} ${styles.Icon__right_secondary}`}
+              >
+                <SuccessLogoSvg width={24} height={24} />
+              </div>
+            ) : null}
           </div>
 
           <label htmlFor="email" className={styles.Label}></label>
@@ -67,14 +89,42 @@ const SignUpForm = () => {
               name="email"
               placeholder="E-mail"
               className={`${styles.Input} ${
-                errors.email && touched.email ? styles.Input__error : null
+                errors.email && touched.email
+                  ? styles.Input__error
+                  : inputState.email && !errors.email
+                  ? styles.Input__success
+                  : null
               }`}
+              onInput={e => {
+                const value = e.target.value;
+                setInputState(prevState => ({
+                  ...prevState,
+                  email: value,
+                }));
+                setFieldTouched('email', true); // Устанавливаем touched в true
+              }}
             />
             <ErrorMessage
               className={`${styles.Message} ${styles.Message__error}`}
               name="email"
               component="div"
             />
+            {errors.email && touched.email ? (
+              <div
+                className={`${styles.Icon} 
+                  ${styles.Icon__right} 
+                  ${styles.Icon__error} 
+                  ${styles.Icon__right_secondary}`}
+              >
+                <ErrorLogoSvg width={24} height={24} />
+              </div>
+            ) : inputState.email && !errors.email ? (
+              <div
+                className={`${styles.Icon} ${styles.Icon__right} ${styles.Icon__success} ${styles.Icon__right_secondary}`}
+              >
+                <SuccessLogoSvg width={24} height={24} />
+              </div>
+            ) : null}
           </div>
 
           <label htmlFor="password" className={styles.Label}></label>
@@ -87,16 +137,34 @@ const SignUpForm = () => {
               className={`${styles.Input} ${
                 errors.password && touched.password
                   ? styles.Input__error
-                  : success
+                  : inputState.password && !errors.password
                   ? styles.Input__success
                   : null
               }`}
+              onInput={e => {
+                const value = e.target.value;
+                setInputState(prevState => ({
+                  ...prevState,
+                  password: value,
+                }));
+                setFieldTouched('password', true); // Устанавливаем touched в true
+              }}
             />
             <ErrorMessage
               className={`${styles.Message} ${styles.Message__error}`}
               name="password"
               component="div"
             />
+
+            {errors.password && touched.password ? (
+              <div className={`${styles.Message} ${styles.Message__error}`}>
+              </div>
+            ) : inputState.password && !errors.password ? (
+              <div className={`${styles.Message} ${styles.Message__success}`}>
+                Password is secure
+              </div>
+            ) : null}
+
             {errors.password && touched.password ? (
               <div
                 className={`${styles.Icon} 
@@ -106,9 +174,9 @@ const SignUpForm = () => {
               >
                 <ErrorLogoSvg width={24} height={24} />
               </div>
-            ) : success ? (
+            ) : inputState.password && !errors.password ? (
               <div
-                className={`${styles.Icon} ${styles.Icon__right} ${styles.Icon__success} ${styles.Icon__right_secondary}`}
+                className={`${styles.Icon} ${styles.Icon__right} ${styles.Icon__success} ${styles.Icon__right_secondary} `}
               >
                 <SuccessLogoSvg width={24} height={24} />
               </div>

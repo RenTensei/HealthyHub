b2d472ac6e1565ac77f6cc14e8fcdf70988c0933
RenTensei <scs.bon7@gmail.com>
2023-09-25 14:49:10 +0000

    Deploying to gh-pages from @ RenTensei/HealthyHub@8d7585fb96679f3973773aa31e8b5b1add670056 🚀

--- assets/arrow-right-liqht-5fd33211.js
+++ assets/arrow-right-liqht-5fd33211.js
@@ -1,4 +1,4 @@
-var Bn=Object.defineProperty;var Wn=(i,t,e)=>t in i?Bn(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var S=(i,t,e)=>(Wn(i,typeof t!="symbol"?t+"":t,e),e);import{r as tt,n as Ks}from"./index-bba96b8d.js";/*!
+var Bn=Object.defineProperty;var Wn=(i,t,e)=>t in i?Bn(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var S=(i,t,e)=>(Wn(i,typeof t!="symbol"?t+"":t,e),e);import{r as tt,n as Ks}from"./index-ae4a7961.js";/*!
  * @kurkle/color v0.3.2
  * https://github.com/kurkle/color#readme
  * (c) 2023 Jukka Kurkela
--- assets/fetchFoodData-1016bc31.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./index-bba96b8d.js";const e=async()=>{try{const{data:o}=await t.get("/user/recomended-food");return o}catch(o){console.log(o)}};export{e as f};

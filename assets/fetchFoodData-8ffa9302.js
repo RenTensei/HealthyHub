d397be21470bf41c9829f0fb56e73b305ae1e61e
RenTensei <scs.bon7@gmail.com>
2023-09-26 18:29:31 +0000

    Deploying to gh-pages from @ RenTensei/HealthyHub@181fd9dbe3b60a4cc32dfe0850710941e490515e 🚀

---
+++ assets/fetchFoodData-8ffa9302.js
@@ -0,0 +1,1 @@
+import{t}from"./index-827630d5.js";const e=async()=>{try{const{data:o}=await t.get("/user/recomended-food");return o}catch(o){console.log(o)}};export{e as f};
--- assets/fetchFoodData-cbdf8be5.js
+++
@@ -1,1 +0,0 @@
-import{t}from"./index-e5c475d6.js";const e=async()=>{try{const{data:o}=await t.get("/user/recomended-food");return o}catch(o){console.log(o)}};export{e as f};

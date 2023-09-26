88b532b5ab108d391366f6b274ff551f29c62acd
RenTensei <scs.bon7@gmail.com>
2023-09-26 19:03:25 +0300

    + improvements

--- src/shared/SharedLayout/Header/modals/ModalWeight.jsx
+++ src/shared/SharedLayout/Header/modals/ModalWeight.jsx
@@ -6,10 +6,12 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUserInfo } from '@/store/features/auth/selectors';
 import { updateUser } from '@/store/features/auth/thunks';
-import moment from 'moment/moment';
+import { formatDate } from '@/utils/formatDate';
+import { toast } from 'react-toastify';
 
 const ModalWeight = ({ open, onClose }) => {
-  const today = moment().format('DD.MM.YYYY');
+  const now = new Date();
+  const today = formatDate(now);
 
   const dispatch = useDispatch();
   const user = useSelector(selectUserInfo);
@@ -18,21 +20,19 @@ const ModalWeight = ({ open, onClose }) => {
   const [width, setWidth] = useState({ width: window.innerWidth });
   const breakpoint = 834;
 
-  const handleChange = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    console.log(weight);
-    console.log(typeof weight);
+
+    if (!weight || weight > 170 || weight < 40) {
+      return toast.warn('Weight should be a number between 40 and 170!');
+    }
+    if (weight === user.weight) return onClose();
+
     dispatch(updateUser({ weight }));
-    // onClose();
+    onClose();
   };
 
-  const handleResize = () => {
-    setWidth({
-      width: window.innerWidth,
-    });
-  };
-
-  console.log(typeof weight);
+  const handleResize = () => setWidth({ width: window.innerWidth });
 
   useEffect(() => {
     window.addEventListener('resize', handleResize);
@@ -94,15 +94,13 @@ const ModalWeight = ({ open, onClose }) => {
                 <form className={styles.header_weight_form}>
                   <input
                     type="number"
-                    onChange={event =>
-                      setWeight(Number(event.currentTarget.value))
-                    }
+                    onChange={event => setWeight(Number(event.target.value))}
                     placeholder="Enter your weight"
                     className={styles.header_modal_input}
                   />
                   <button
                     className={styles.header_modal_bnt}
-                    onClick={handleChange}
+                    onClick={handleSubmit}
                   >
                     {' '}
                     Confirm
@@ -169,7 +167,7 @@ const ModalWeight = ({ open, onClose }) => {
                   />
                   <button
                     className={styles.header_modal_bnt}
-                    onClick={handleChange}
+                    onClick={handleSubmit}
                   >
                     {' '}
                     Confirm
